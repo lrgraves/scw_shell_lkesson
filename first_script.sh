2fd5005101ad4eb80bd04a94b7a6a043a0cacef0
lrgraves <lloganl123@gmail.com>
2017-02-25 12:21:23 -0700

    We have added the help option to the first_script.sh file

--- first_script.sh
+++ first_script.sh
@@ -1,3 +1,8 @@
 # This script is used to count number of characters in a file
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    echo "Usage: sh $0 input_file output_file"
+    exit
+fi
+
 head -5 $1 | sort > $2
 wc -c $2 > $2
